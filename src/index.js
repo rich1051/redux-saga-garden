bbb1610874f4249cbfb075cde33c2c32c4c741c3
Will Richardson <rich1051@umn.edu>
2023-06-14 21:33:01 -0500

    troubleshooting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
+import logger from 'redux-logger';
+import createSagaMiddleware from "redux-saga";
+import { takeLatest, put } from "redux-saga/effects";
+import axios from "axios";
 import App from './App';
 
 // this startingPlantArray should eventually be removed
@@ -21,8 +24,40 @@ const plantList = (state = startingPlantArray, action) => {
   }
 };
 
+function* fetchPlant () {
+  try {
+    // hold response from server in plantResponse:
+    const plantResponse = yield axios.get("/api/plant");
+    // ...after server responds, then this generator function/saga continues
+    yield put({ type: "ADD_PLANT", payload: plantResponse.data });
+  } catch (error) {
+    console.log("Error fetching plants:", error);
+  }
+}
+
+function* postPlant(action) {
+  try {
+    console.log("firstSaga was hit with action:", action);
+// hold response from server in newElement:
+    yield axios.post("/api/plant", action.payload);
+// ...after server responds, then this generator function/saga continues
+    yield put({ type: "FETCH_PLANT" });
+  } catch (error) {
+    console.log("Error posting plant:", error);
+  }
+}
+
+
+function* rootSaga() {
+    yield takeLatest("FETCH_PLANT", fetchPlant);
+    yield takeLatest("POST_PLANT", postPlant);
+  }
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
